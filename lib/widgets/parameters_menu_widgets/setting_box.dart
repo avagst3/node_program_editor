5ed35afc4fb60bc007e086bc2b783809814b3d95
avgvst3 <lacheteaua@gmail.com>
2025-04-02 15:36:04 +0200

    correct settings navigation block

--- lib/widgets/parameters_menu_widgets/setting_box.dart
+++ lib/widgets/parameters_menu_widgets/setting_box.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
@@ -71,7 +70,7 @@ class SettingBox extends StatelessWidget {
                             ),
                           ),
                           SettingBottomBar(
-                            event: SettingsNavigationToDiagramSettings(),
+                            event: SettingsNavigationToBlockSettings(),
                             buttonLabel: "Bloc settings",
                           ),
                         ],
